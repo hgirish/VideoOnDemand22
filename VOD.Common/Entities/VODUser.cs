b378cc5670bd78ffc8828c99780c5c9a16cbc16d
hgirish <girish@post.com>
2019-06-30 12:43:59 -0700

    Entities added

--- VOD.Common/Entities/VODUser.cs
+++ VOD.Common/Entities/VODUser.cs
@@ -3,11 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Security.Claims;
-using System.Text;
 
 namespace VOD.Common.Entities
 {
-   public class VODUser : IdentityUser
+    public class VODUser : IdentityUser
     {
         public string Token { get; set; }
         public DateTime TokenExpires { get; set; }
