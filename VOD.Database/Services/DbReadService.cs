946d8bb56819df483001ae8f3fb25d329cc3e417
hgirish <girish@post.com>
2019-06-30 21:15:53 -0700

    DbReadService and ListExtensions

---
+++ VOD.Database/Services/DbReadService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using VOD.Database.Contexts;
+
+namespace VOD.Database.Services
+{
+    public class DbReadService : IDbReadService
+    {
+        private readonly VODContext _db;
+
+        public DbReadService(VODContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> AnyAsync<TEntity>(Expression<Func<TEntity, bool>> expression) where TEntity : class
+        {
+            return await _db.Set<TEntity>().AnyAsync(expression);
+        }
+
+        public async  Task<List<TEntity>> GetAsync<TEntity>() where TEntity : class
+        {
+            return await _db.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<List<TEntity>> GetAsync<TEntity>(Expression<Func<TEntity, bool>> expression) where TEntity : class
+        {
+            return await _db.Set<TEntity>().Where(expression).ToListAsync();
+        }
+
+        public void Include<TEntity>() where TEntity : class
+        {
+            var propertyNames = _db.Model
+                .FindEntityType(typeof(TEntity))
+                .GetNavigations()
+                .Select(e => e.Name);
+
+            foreach (var name in propertyNames)
+            {
+                _db.Set<TEntity>().Include(name).Load();
+            }
+        }
+
+        public void Include<TEntity1, TEntity2>()
+            where TEntity1 : class
+            where TEntity2 : class
+        {
+            Include<TEntity1>();
+            Include<TEntity2>();
+        }
+
+        public async Task<TEntity> SingleAsync<TEntity>(Expression<Func<TEntity, bool>> expression) where TEntity : class
+        {
+            return await _db.Set<TEntity>().Where(expression).SingleOrDefaultAsync();
+        }
+    }
+}
--- VOD.UI/Controllers/HomeController.cs
+++ VOD.UI/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using VOD.Common.Entities;
+using VOD.Database.Services;
 using VOD.UI.Models;
 
 namespace VOD.UI.Controllers
@@ -9,13 +11,25 @@ namespace VOD.UI.Controllers
     public class HomeController : Controller
     {
         private readonly SignInManager<VODUser> _signInManager;
+       // private readonly IDbReadService _db;
 
-        public HomeController(SignInManager<VODUser> signInManager)
+        public HomeController(SignInManager<VODUser> signInManager
+           // , IDbReadService db
+            )
         {
             _signInManager = signInManager;
+           // _db = db;
         }
         public IActionResult Index()
         {
+            //_db.Include<Download>();
+            //_db.Include<Module, Course>();
+            //var result1 = await _db.SingleAsync<Download>(d => d.Id.Equals(3));
+            //var result2 = await _db.GetAsync<Download>();
+            //var result3 = await _db.GetAsync<Download>(d => d.ModuleId.Equals(1));
+           // var result4 = await _db.AnyAsync<Download>(d => d.ModuleId.Equals(99));
+           // var result5 = await _db.AnyAsync<Download>(d => d.ModuleId.Equals(1));
+
             if (!_signInManager.IsSignedIn(User))
             {
                 return RedirectToPage("/Account/Login", new { Area = "Identity" });
