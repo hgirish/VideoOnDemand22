6838116b5ea68cf947d45a7176c88f0b85c7a385
Girish <girish@post.com>
2019-07-03 14:51:23 -0700

    BtnTagHelper

---
+++ VOD.Admin/TagHelpers/BtnTagHelper.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace VOD.Admin.TagHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("btn")]
+    public class BtnTagHelper : AnchorTagHelper
+    {
+        public string Icon { get; set; } = string.Empty;
+        const string btnPrimary = "btn-primary";
+        const string btnDanger = "btn-danger";
+        const string btnDefault = "btn-default";
+        const string btnInfo = "btn-info";
+        const string btnSucess = "btn-success";
+        const string btnWarning = "btn-warning";
+        // Google's Material Icons provider name
+        const string iconProvider = "material-icons";
+        public BtnTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+
+        }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+            output.TagName = "a";
+          
+
+            var aspPageAttribute = context.AllAttributes.SingleOrDefault(p =>
+            p.Name.ToLower().Equals("asp-page"));
+
+            var classAttribute = context.AllAttributes.SingleOrDefault(p =>
+            p.Name.ToLower().Equals("class"));
+
+            var buttonStyle = btnDefault;
+
+            if (aspPageAttribute != null)
+            {
+                var pageValue = aspPageAttribute.Value.ToString().ToLower();
+                buttonStyle = pageValue.Equals("create") ? btnPrimary :
+                    pageValue.Equals("delete") ? btnDanger :
+                    pageValue.Equals("edit") ? btnSucess :
+                    pageValue.Equals("index") ? btnPrimary :
+                    pageValue.Equals("details") ? btnInfo :
+                    pageValue.Equals("/index") ? btnWarning :
+                    pageValue.Equals("error") ? btnDanger :
+                    btnDefault;
+
+            }
+            var bootstrapClasses = $"btn-sm {buttonStyle}";
+
+            if (classAttribute != null)
+            {
+                var css = classAttribute.Value.ToString();
+                if (!css.ToLower().Contains("btn-")) {
+                    output.Attributes.Remove(classAttribute);
+                    classAttribute = new TagHelperAttribute(
+                    "class", $"{css} {bootstrapClasses}");
+                    output.Attributes.Add(classAttribute);
+                }
+            }
+            else
+            {
+                output.Attributes.Add("class", bootstrapClasses);
+            }
+
+            base.Process(context, output);
+        }
+    }
+}
