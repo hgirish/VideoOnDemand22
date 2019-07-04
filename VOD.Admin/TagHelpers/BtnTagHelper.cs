12003a52fd42e4ba20b1a4e2c3ad5f574937a3e9
hgirish <girish@post.com>
2019-07-03 21:58:41 -0700

    BtnTagHelper complete

--- VOD.Admin/TagHelpers/BtnTagHelper.cs
+++ VOD.Admin/TagHelpers/BtnTagHelper.cs
@@ -1,31 +1,32 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace VOD.Admin.TagHelpers
 {
-    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
     [HtmlTargetElement("btn")]
     public class BtnTagHelper : AnchorTagHelper
     {
         public string Icon { get; set; } = string.Empty;
+
         const string btnPrimary = "btn-primary";
         const string btnDanger = "btn-danger";
         const string btnDefault = "btn-default";
         const string btnInfo = "btn-info";
         const string btnSucess = "btn-success";
         const string btnWarning = "btn-warning";
+
         // Google's Material Icons provider name
         const string iconProvider = "material-icons";
+
+        // This constructor is needed for AnchorTagHelper inheritance
         public BtnTagHelper(IHtmlGenerator generator) : base(generator)
         {
-
         }
+
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (context == null)
@@ -76,6 +77,30 @@ namespace VOD.Admin.TagHelpers
             {
                 output.Attributes.Add("class", bootstrapClasses);
             }
+
+            if (!Icon.Equals(string.Empty))
+            {
+                var childContext = output.GetChildContentAsync().Result;
+                var content = childContext.GetContent().Trim();
+                if (content.Length > 0)
+                {
+                    content = $"&nbsp;{content}";
+                }
+                output.Content.SetHtmlContent($"<i class='{iconProvider}' style='display: inline-flex; vertical-align: top; line-height: inherit; font-size: medium;'>{Icon}</i> <span style='font-size: medium;'>{content}</span>");
+
+            }
+            var style = context.AllAttributes.SingleOrDefault(s =>
+            s.Name.ToLower().Equals("style"));
+            var styleValue = style == null ? "" : style.Value;
+
+            var newStyle = new TagHelperAttribute("style",
+                $"{styleValue} display:inline-flex; border-radius:0px; text-decoration:none;");
+
+            if (style != null)
+            {
+                output.Attributes.Remove(style);
+            }
+            output.Attributes.Add(newStyle);
 
             base.Process(context, output);
         }
