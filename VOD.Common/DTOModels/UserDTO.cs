bfa81188e045aaa0a57e3bb9e4c4a63dccc16d3a
hgirish <girish@post.com>
2019-07-05 17:33:38 -0700

    ButtonDTO UserDTO

---
+++ VOD.Common/DTOModels/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VOD.Common.DTOModels
+{
+   public class UserDTO
+    {
+        [Required]
+        [Display(Name ="User Id")]
+        public string Id { get; set; }
+    }
+}
