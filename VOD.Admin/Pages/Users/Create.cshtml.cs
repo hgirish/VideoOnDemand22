7aade23dd3e9c4c69a08276d11f9e22cfc73e5e5
Girish <girish@post.com>
2019-07-08 14:24:49 -0700

    Users create edit delete

---
+++ VOD.Admin/Pages/Users/Create.cshtml.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VOD.Common.DTOModels;
+using VOD.Database.Services;
+
+namespace VOD.Admin.Pages.Users
+{
+    [Authorize(Roles = "Admin")]
+    public class CreateModel : PageModel
+    {
+        private readonly IUserService _userService;
+        [BindProperty]
+        public RegisterUserDTO Input { get; set; } =
+            new RegisterUserDTO();
+        [TempData]
+        public string Alert { get; set; }
+
+        public CreateModel(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public void OnGet()
+        {
+
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.AddUserAsync(Input);
+                if (result.Succeeded)
+                {
+                    Alert = $"Created a new account for {Input.Email}.";
+                    return RedirectToPage("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return Page();
+        }
+    }
+}
---
+++ VOD.Admin/Pages/Users/Index.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VOD.Common.DTOModels;
+using VOD.Database.Services;
+
+namespace VOD.Admin.Pages.Users
+{
+    [Authorize(Roles ="Admin")]
+    public class IndexModel : PageModel
+    {
+        private readonly IUserService _userService;
+        public IEnumerable<UserDTO> Users = new List<UserDTO>();
+        [TempData]
+        public string Alert { get; set; }
+
+        public IndexModel(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public async Task OnGetAsync()
+        {
+            Users = await _userService.GetUsersAsync();
+        }
+    }
+}
