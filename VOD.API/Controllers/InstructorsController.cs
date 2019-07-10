75e97c18830ce172899a3a46923c6ef21b57ca3d
hgirish <girish@post.com>
2019-07-09 18:04:42 -0700

    instructors api controller

---
+++ VOD.API/Controllers/InstructorsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using VOD.Common.DTOModels.Admin;
+using VOD.Common.Entities;
+using VOD.Common.Services;
+
+namespace VOD.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstructorsController : ControllerBase
+    {
+        private readonly IAdminService _db;
+        private readonly LinkGenerator _linkGenerator;
+
+        public InstructorsController(IAdminService db, LinkGenerator linkGenerator)
+        {
+            _db = db;
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet()]
+        public async Task<ActionResult<List<InstructorDTO>>> Get(bool include = false)
+        {
+            try
+            {
+                return await _db.GetAsync<Instructor, InstructorDTO>(include);
+            }
+            catch 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<InstructorDTO>> Get(int id, bool include = false)
+        {
+            try
+            {
+                var dto = await _db.SingleAsync<Instructor, InstructorDTO>(s =>
+                s.Id.Equals(id), include);
+                if (dto == null)
+                {
+                    return NotFound();
+                }
+                return dto;
+            }
+            catch 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");                
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<InstructorDTO>> Post(InstructorDTO model)
+        {
+            try
+            {
+                if (model == null)
+                {
+                    return BadRequest("No entity provided");
+                }
+                var id = await _db.CreateAsync<InstructorDTO, Instructor>(model);
+
+                var dto = await _db.SingleAsync<Instructor, InstructorDTO>(s =>
+                s.Id.Equals(id));
+
+                if (dto == null)
+                {
+                    return BadRequest("Unable to add the entity");
+                }
+                var uri = _linkGenerator.GetPathByAction("Get", "Instructors", new { id });
+                return Created(uri, dto);
+                
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to add entity");
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, InstructorDTO model)
+        {
+            try
+            {
+                if (model == null)
+                {
+                    return BadRequest("No entity provided");
+                }
+                if (!id.Equals(model.Id))
+                {
+                    return BadRequest("Differing ids");
+                }
+                var exists = await _db.AnyAsync< Instructor>(a => a.Id.Equals(id));
+                if (!exists)
+                {
+                    return NotFound("Could not find entity");
+                }
+                var success = await _db.UpdateAsync<InstructorDTO, Instructor>(model);
+                if (success)
+                {
+                    return NoContent();
+                }
+               
+                    
+               
+
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to update entity");
+            }
+            return BadRequest("Unable to update the entity");
+        }
+    }
+}
